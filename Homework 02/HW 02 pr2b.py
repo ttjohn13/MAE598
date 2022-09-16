d144bca8d6f426074e444bd415a7552ddd82397c
ttjohn13 <ttjohn13@asu.edu>
2022-09-16 14:23:00 -0700

    Completed 2b gradient decent problem and graphing. Need to still do newton method

--- Homework 02/HW 02 pr2b.py
+++ Homework 02/HW 02 pr2b.py
@@ -1,8 +1,8 @@
 # %%
+import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as ppl
-from scipy import optimize as opt
 
 
 # defines the distance squared function
@@ -45,27 +45,36 @@ def gradient_decent_inexact_line_search(function, gradient, x0, t, tollerance):
     counter = 0
     g0 = gradient(x0)
     g0norm = np.linalg.norm(g0)
+    x0track = x0
+    distance_track = np.array([function(x0)])
     while g0norm > tollerance and counter < 100:
         counter += 1
         x0 = inexact_line_search(function, g0, x0, t)
         g0 = gradient(x0)
         g0norm = np.linalg.norm(g0)
+        x0track = np.vstack((x0track, x0))
+        distance0 = np.array([function(x0)])
+        distance_track = np.concatenate((distance_track, distance0))
     success = g0norm < tollerance
     distance = function(x0)
     print("was a success: " + str(success) + "\nx values = "), print(x0), print("\nMinimum distance: "), print(distance)
-    return x0
+    return x0, x0track, distance_track
 
 
 x0 = np.array([1, 1])
-xtrial = gradient_decent_inexact_line_search(distance_sq, distance_sq_g, x0, 0.5, 1e-3)
+(x_final, x_track, distance_track) = gradient_decent_inexact_line_search(distance_sq, distance_sq_g, x0, 0.5, 1e-3)
+iteration_count = np.count_nonzero(distance_track, axis=0)
+print('completed in ' + str(iteration_count) + ' iterations')
 
-'''
-x0 = [1, 2]
-resgrad = opt.minimize(distance_sq,x0,method='BFGS' , jac= distance_sq_g,options={'display':True, 'return_all':True})
-xtrial = resgrad.allvecs
-xfinal = resgrad.x
-resgrad
-print(xfinal)
-resnewton = opt.minimize(distance_sq,x0, method='Newton-CG', jac=distance_sq_g,hess=distance_sq_H, options={'xtol':1e-8, 'disp':True})
-xfinal =resnewton.x
-print(xfinal) '''
+x_axis = np.arange(iteration_count - 1)
+y_axis = distance_track[:iteration_count - 1] - distance_track[-1]
+
+fig, ax = ppl.subplots()
+ax.set_yscale('log')
+ax.plot(x_axis, y_axis, label='Gradient Decent')
+plt.xlabel('iteration')
+plt.ylim(bottom=10 ** -8)
+plt.ylabel('Error')
+plt.grid(True)
+plt.title('Convergence Plot')
+plt.show()
