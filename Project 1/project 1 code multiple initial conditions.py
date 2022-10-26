405e0f87bb9ad1230bd657e651e332f5af835afd
ttjohn13 <ttjohn13@asu.edu>
2022-10-25 23:05:49 -0700

    Working stable multiple initial conditions. Need to work on visualization

---
+++ Project 1/project 1 code multiple initial conditions.py
@@ -0,0 +1,192 @@
+import logging
+import math
+import random
+import numpy as np
+import time
+
+import torch
+import torch as t
+import torch.nn as nn
+from torch import optim
+from torch.nn import utils
+import matplotlib.pyplot as plt
+
+t.manual_seed(0)
+
+logger = logging.getLogger(__name__)
+
+FRAME_TIME = 1.0
+GRAVITY_ACCEL = 9.81 / 1000
+BOOST_ACCEL = 14.715 / 1000
+L_center_of_gravity = 5 / 1000
+
+PLATFORM_WIDTH = 25 / 1000
+
+
+class Dynmaics(nn.Module):
+
+    def __init__(self):
+        super(Dynmaics, self).__init__()
+
+    @staticmethod
+    def forward(state, action):
+        """
+        action[0]: thrust
+        action[1]: phi of thrust
+        state[0] = y
+        state[1] = y_dot
+        state[2] = x
+        state[3] = x_dot
+        state[4] = theta
+        state[5] = omega
+        """
+        delta_state_gravity = t.tensor([0., -GRAVITY_ACCEL * FRAME_TIME, 0., 0., 0., 0])
+        N = len(state)
+        # Thrust Calculations
+        state_tensor = torch.zeros((N, 6))
+        state_tensor[:, 1] = torch.cos(state[:, 4] + action[:, 1])
+        state_tensor[:, 3] = torch.sin(state[:, 4] + action[:, 1])
+        delta_state_thrust = BOOST_ACCEL * FRAME_TIME * t.mul(state_tensor, action[:, 0].reshape(-1, 1))
+
+        state_tensor_angle = t.zeros((N, 6))
+        state_tensor_angle[:, 5] = 4 / L_center_of_gravity * t.sin(action[:, 1]) * action[:, 0]
+        delta_state_angle = BOOST_ACCEL * FRAME_TIME * torch.mul(state_tensor_angle, action[:, 1].reshape(-1, 1))
+
+        state = state + delta_state_thrust + delta_state_gravity + delta_state_angle
+
+        step_mat = t.tensor([[1., FRAME_TIME, 0., 0., 0., 0.],
+                             [0., 1., 0., 0., 0., 0.],
+                             [0., 0., 1., FRAME_TIME, 0., 0.],
+                             [0., 0., 0., 1., 0., 0.],
+                             [0., 0., 0., 0., 1., FRAME_TIME],
+                             [0., 0., 0., 0., 0., 1]])
+        state = t.t(t.matmul(step_mat, state.t()))
+
+        return state
+
+
+class Controller(nn.Module):
+
+    def __init__(self, dim_input, dim_hidden, dim_output):
+        """
+        dim_input: # of system states
+        dim_output: # of actions
+        dim_hidden: up to me
+        """
+
+        super(Controller, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(dim_input, dim_hidden),
+            nn.Tanh(),
+            nn.Linear(dim_hidden, dim_output),
+            nn.Sigmoid(),
+            nn.Linear(dim_output, dim_input),
+            nn.Tanh(),
+            nn.Linear(dim_input, dim_output),
+            nn.Tanh()
+        )
+
+    def forward(self, state):
+        action = self.network(state)
+        action = action / 2 + t.tensor([0.5, 0.])
+        return action
+
+
+class Simulation(nn.Module):
+
+    def __init__(self, controller, dynamics, T):
+        super(Simulation, self).__init__()
+        self.state = self.initialize_state()
+        self.controller = controller
+        self.dynamics = dynamics
+        self.T = T
+        self.action_trajectory = []
+        self.state_trajectory = []
+
+    def forward(self, state):
+        self.action_trajectory = []
+        self.state_trajectory = []
+        for _ in range(T):
+            action = self.controller.forward(state)
+            state = self.dynamics.forward(state, action)
+            self.action_trajectory.append(action)
+            self.state_trajectory.append(state)
+        return self.error(state, self.state_trajectory)
+
+    @staticmethod
+    def initialize_state():
+        state = [[1.000, -0.01, .025, -0.001, 0., -0.01],
+                 [0.950, 0., -0.025, -0.002, 0.03, 0.01]]# need initial conditions
+        return t.tensor(state, requires_grad=False).float()
+
+    def error(self, state, state_trajectory):
+        termination_error = 10 * (state[:, 0] - L_center_of_gravity) ** 2 + 10 * state[:, 1] ** 2 + state[:, 2] ** 2 + state[:, 3] ** 2 + state[:, 4] ** 2 + 4 * state[:, 5] ** 2
+        termination_error = t.sum(termination_error)
+        stack_state_traj = t.stack(state_trajectory)
+        x_location = stack_state_traj[:-1, :, 2]
+        x_location_error_squared = t.sum(x_location**2, (1, 0))
+        angle = stack_state_traj[:-1, :, 4]
+        angle_error_squared = t.sum(angle**2, (0, 1))
+        y_speed = stack_state_traj[-5:-1, :, 1]
+        y_speed_error_squared = t.sum(y_speed**2, (1, 0))
+        transition_error = 0.03 * x_location_error_squared + 0.1 * angle_error_squared + 0.2 * y_speed_error_squared
+
+        return termination_error + transition_error
+
+
+class Optimize:
+    def __init__(self, simulation):
+        self.simulation = simulation
+        self.parameters = simulation.controller.parameters()
+        self.optimizer = optim.LBFGS(self.parameters, lr=0.08)
+
+    def step(self):
+        def closure():
+            loss = self.simulation(self.simulation.state)
+            self.optimizer.zero_grad()
+            loss.backward()
+            return loss
+
+        self.optimizer.step(closure)
+        return closure()
+
+    def train(self, epochs):
+        for epoch in range(epochs):
+            loss = self.step()
+            print('[%d] loss: %.5f' % (epoch + 1, loss))
+            #self.visualize()
+
+    """
+    def visualize(self):
+        data = np.array([self.simulation.state_trajectory[i].detach().numpy() for i in range(self.simulation.T)])
+        y = data[:, 0]
+        y_dot = data[:, 1]
+        x = data[:, 2]
+        x_dot = data[:, 3]
+        theta = data[:, 4]
+        omega = data[:, 5]
+        fig, axs = plt.subplots(3, 1)
+        axs[0].plot(y, y_dot)
+        axs[0].set_xlabel('Vertical Distance to Ground')
+        axs[0].set_ylabel('Vertical Speed')
+        axs[1].plot(y, x, label='Horizontal Distance from Platform')
+        axs[1].plot(y, x_dot, label='Horizontal Speed')
+        axs[1].set_xlabel('Vertical Distance to Ground')
+        axs[1].set_ylabel('x and x_dot')
+        axs[2].plot(y, theta, label='Orientation')
+        axs[2].plot(y, omega, label='Angular Speed')
+        axs[2].set_xlabel('Vertical Distance to Ground')
+        axs[2].set_ylabel('Theta and Omega')
+        plt.show()
+        plt.legend
+    """
+
+T = 20
+dim_input = 6
+dim_hidden = 12
+dim_output = 2
+d = Dynmaics()
+c = Controller(dim_input, dim_hidden, dim_output)
+s = Simulation(c, d, T)
+o = Optimize(s)
+o.train(50)
